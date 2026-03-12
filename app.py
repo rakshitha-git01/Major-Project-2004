57f7773da616940731decc78bd7a71ac86c082ed
rakshitha-git01 <rakshithakanneboina@gmail.com>
2026-03-12 21:31:27 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -996,8 +996,10 @@ def predict():
     )
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
 #----- for mapping + heatmap-----
 
